1c5b4236750760736a9560e0b5a99bf1d848d500
maksutgavaz <mgavaz94@gmail.com>
2016-06-03 18:38:07 +0200

    footer in ventures page

--- application/views/ventures.php
+++ application/views/ventures.php
@@ -71,8 +71,8 @@
 						</ul>				
 						<div class="separator mt-10"></div>
 						<ul class="nav navbar-nav text-center">
-							<li class="active"><a href="/Main/landing">Home</a></li>
-							<li><a href="/Main/ventures">Ventures</a></li>
+							<li><a href="/Main/landing">Home</a></li>
+							<li class="active"><a href="/Main/ventures">Ventures</a></li>
 							<li><a href="/Main/teams">Teams</a></li>
 						</ul>
 					</nav>
@@ -417,6 +417,59 @@
 					</div>
 				</div>		
 			</div>
+
+								<!-- footer top start -->
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="call-to-action text-center">
+								<div class="row">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			<!-- footer top end -->
+
+			<!-- footer start (Add "dark" class to #footer in order to enable dark footer) -->
+			<footer id="footer" class="clearfix dark">
+
+				<!-- .footer start -->
+				<!-- ================ -->
+				<div class="footer">
+					<div class="container">
+						<div class="footer-inner">
+							<div class="row">
+								<div class="col-md-6 col-md-offset-3">
+									<div class="footer-content text-center padding-ver-clear">
+										<div class="logo-footer"><img id="logo-footer" class="center-block" src="/assets/images/Logo_origami.png" alt="Logo_Enactus_Origami"></div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus illo vel dolorum soluta consectetur doloribus sit. Delectus non tenetur odit dicta vitae debitis suscipit doloribus. Lorem ipsum dolor sit amet, illo vel dolorum soluta consectetur.</p>
+										<ul class="list-inline mb-20">
+											<li><i class="text-default fa fa-map-marker pr-5"></i>One infinity loop, 54100</li>
+											<li><a href="tel:+00 1234567890" class="link-dark"><i class="text-default fa fa-phone pl-10 pr-5"></i>+00 1234567890</a></li>
+											<li><a href="mailto:info@theproject.com" class="link-dark"><i class="text-default fa fa-envelope-o pl-10 pr-5"></i>info@theproject.com</a></li>
+										</ul>
+										<ul class="social-links circle animated-effect-1 margin-clear">
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+											<li class="instagram"><a target="_blank" href="http://www.instagram.com"><i class="fa fa-instagram"></i></a></li>
+											<li class="googleplus"><a target="_blank" href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
+											<li class="linkedin"><a target="_blank" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
+										</ul>
+										<div class="separator"></div>
+										<p class="text-center margin-clear">Copyright © 2016 The Project by <a target="_blank" href="http://htmlcoder.me">HtmlCoder</a>. All Rights Reserved</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- .footer end -->
+
+			</footer>
+			<!-- ================ -->
+			
+			<!-- footer top end -->
+	<!-- </div> -->
 
 			<!-- JavaScript files placed at the end of the document so the pages load faster -->
 			<!-- ================================================== -->
