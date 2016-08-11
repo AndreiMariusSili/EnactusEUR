584de40ef5c9893af9869793ea1bdacee1d2f6cb
Andrei Sili <andrei.sili1994@gmail.com>
2016-08-11 20:53:03 +0300

    console error test fix

--- application/views/ventures.php
+++ application/views/ventures.php
@@ -381,12 +381,12 @@
 			<!-- Initialization of Plugins -->
 			<script type="text/javascript" src="/assets/js/template.js"></script>
 			<!-- jQuery Revolution Slider  -->
-			<script type="text/javascript" src="/assets/plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<!-- 			<script type="text/javascript" src="/assets/plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
 			<script type="text/javascript" src="/assets/plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
 			<script type="text/javascript" src="/assets/plugins/rs-plugin-5/js/extensions/revolution.extension.video.min.js"></script>
 			<script type="text/javascript" src="/assets/plugins/rs-plugin-5/js/extensions/revolution.extension.slideanims.min.js"></script>
 			<script type="text/javascript" src="/assets/plugins/rs-plugin-5/js/extensions/revolution.extension.layeranimation.min.js"></script>
-			<script type="text/javascript" src="/assets/plugins/rs-plugin-5/js/extensions/revolution.extension.navigation.min.js"></script>
+			<script type="text/javascript" src="/assets/plugins/rs-plugin-5/js/extensions/revolution.extension.navigation.min.js"></script> -->
 			<!-- Jasny Bootstrap  -->
 			<script type="text/javascript" src="/assets/plugins/jasny-bootstrap/js/jasny-bootstrap.js"></script>
 			<!-- Custom Scripts -->
