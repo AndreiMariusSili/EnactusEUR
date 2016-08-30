cf62d02eccd94eb91e5aab0f760ec971a0fedc07
Andrei Sili <andrei.sili1994@gmail.com>
2016-08-31 01:14:28 +0200

    added local development database switch functionality

--- application/config/database.php
+++ application/config/database.php
@@ -70,11 +70,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'production';
 $query_builder = TRUE;
 
+$db['development'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'enactusdb',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
 
-$db['default'] = array(
+$db['production'] = array(
 	'dsn'	=> '',
 	'hostname' => '37.139.25.104',
 	'username' => 'forge',
